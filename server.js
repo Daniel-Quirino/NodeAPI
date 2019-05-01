e3a85b1abdfad96484ba7f92256e489cbf32113a
Daniel Pires Quirino <danielpires6@hotmail.com>
2019-05-01 18:08:32 -0300

    Normalizando porta de acesso para o servidor

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-'use strict'
-
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
 
 const app = express();
-const port = 300;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port',port);
 
 const server = http.createServer(app);
@@ -22,3 +20,18 @@ app.use('/', route);
 
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+  
+    if (port >= 0) {
+      return port;
+    }
+  
+    return false;
+  }
+  
