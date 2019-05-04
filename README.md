790181958557287289574187cf6c128ccfccedab
Daniel Pires Quirino <danielpires6@hotmail.com>
2019-05-03 22:33:09 -0300

    Fazendo coneção com o Banco de Dados

---
+++ README.md
@@ -0,0 +1,4 @@
+# MEAN-Stack-With-Angular-2
+
+01. mlab.com
+Nesse site você consegue simular um Banco de Dados NoSQL online (É necessário ter uma conta para acessar os servições neles disponibilizados)
--- src/app.js
+++ src/app.js
@@ -2,15 +2,22 @@
 
 const express = require('express');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express();
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 
+mongoose.connect('mongodb+srv://daniel:vapara123@cluster0-dydox.mongodb.net/admin?replicaSet=Cluster0-shard-0&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1')
+
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+ 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
 
